327f106a01c26de1097ca171c9d1cf516b30a4f3
Petros Shumie <shumie.peter@gmail.com>
2020-01-25 12:36:48 -0500

    re deploying to heroku.

--- OilPriceIndex/models/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    Oil: require("./oil")
-};
--- OilPriceIndex/routes/api/prices.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const priceController = require("../../controllers/priceController");
-
-// Matches with "/api/prices"
-router
-    .route("/")
-    .get(priceController.findAll);
-
-    module.exports = router;
